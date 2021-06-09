dbba1f795a007aef72817c0814e355e924ff55fc
Henry Romero <hromerp22@gmail.com>
2021-06-09 23:06:20 +0200

    agregado de ganador

--- src/componentes/Square.js
+++ src/componentes/Square.js
@@ -1,19 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const style = {
-    background: 'lightgreen',
-    border: '2px solid darkgray',
-    fontSize: '40px',
-    cursor: 'pointer',
-    outline: "none",
-
-
+  background: "lightblue",
+  border: "2px solid darkgray",
+  fontSize: "40px",
+  cursor: "pointer",
+  outline: "none",
 };
 
 const Square = ({ value, onClick }) => (
-    <button style={style} onClick={onClick}>
-        {value}
-    </button>
+  <button style={style} onClick={onClick}>
+    {value}
+  </button>
 );
 
 export default Square;
--- src/vistas/Registro.js
+++ src/vistas/Registro.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import './Registro.css';
-
+import React from "react";
+import "./Registro.css";
 
 function Registro({ iniciarJuego }) {
-    return <div>
-        <h4>Pick A Weapon</h4>
+  return (
+    <div>
+      <h4>Elige</h4>
 
-        <div className="cajaUsuarios">
-            <h4>CHOOSE YOUR WEAPON</h4>
-            <div>
-                <input type="text" placeholder="participante 1" />
-                <input type="text" placeholder="participante 2" />
-            </div>
-            <div className="botones">
-                <button onClick={() => iniciarJuego(true)} className="x">x</button>
-                <button onClick={() => iniciarJuego(false)} className="o">o</button>
-            </div>
-
+      <div className="cajaUsuarios">
+        <h4>CHOOSE YOUR WEAPON</h4>
+        <div>
+          <input type="text" placeholder="participante 1" />
+          <input type="text" placeholder="participante 2" />
         </div>
+        <div className="botones">
+          <button onClick={() => iniciarJuego(true)} className="x">
+            x
+          </button>
+          <button onClick={() => iniciarJuego(false)} className="o">
+            o
+          </button>
+        </div>
+      </div>
     </div>
-
+  );
 }
 
 export default Registro;
